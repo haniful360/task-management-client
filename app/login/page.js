1615d77aa604d9954bf981e816db7547779ea618
haniful360 <haniful360@gmail.com>
2025-01-05 13:57:53 +0600

    add user successfully login

--- app/login/page.js
+++ app/login/page.js
@@ -1,6 +1,46 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Login = () => {
+  // const [formData, setFormData] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPassword_confirmation] = useState("");
+  // const handleOnchange = (e) => {
+  //   console.log(e.target.value);
+  //   const { name, value } = e.target;
+  //   console.log(name, value);
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: value,
+  //   }));
+  // };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      email,
+      password,
+      password_confirmation
+    };
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <div>
       <div className="bg-gray-100 flex items-center justify-center min-h-screen">
@@ -8,13 +48,15 @@ const Login = () => {
           <h2 className="text-2xl font-bold text-center text-blue-600">
             Login
           </h2>
-          <form className="space-y-4">
+          <form onSubmit={handleOnSubmit} className="space-y-4">
             <div>
               <label className="block text-sm font-medium text-gray-700">
                 Email
               </label>
               <input
                 type="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Enter your email"
                 className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-blue-500 focus:border-blue-500"
               />
@@ -25,6 +67,20 @@ const Login = () => {
               </label>
               <input
                 type="password"
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter your password"
+                className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                onChange={(e) => setPassword_confirmation(e.target.value)}
                 placeholder="Enter your password"
                 className="w-full mt-1 px-4 py-2 border rounded-lg focus:ring-blue-500 focus:border-blue-500"
               />
